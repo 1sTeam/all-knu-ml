a9c8a5e0e71ba3c6b0bd287681811bb3e31feadc
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-09-13 20:52:07 +0900

    공지사항 게시글 내용 text 크롤링 및 dataframe csv 저장

--- ml.py
+++ ml.py
@@ -3,46 +3,55 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.62 Safari/537.36'}
+def all_pages_crawling():
+    headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.62 Safari/537.36'}
+    #1 부터 수집하면 오래걸림 공지사항이 494페이지까지 있어서 테스트용으로 490페이지부터 시작
+    n = 490
+    noticelist = list()
 
-n = 490
-noticelist = list()
-while(1):
-    url = "https://web.kangnam.ac.kr/menu/f19069e6134f8f8aa7f689a4a675e66f.do?paginationInfo.currentPageNo="+str(n)+"&searchMenuSeq=0&searchType=&searchValue="
-    n+=1
-    response = requests.get(url, headers=headers)
+    while(1):
+        url = "https://web.kangnam.ac.kr/menu/f19069e6134f8f8aa7f689a4a675e66f.do?paginationInfo.currentPageNo="+str(n)+"&searchMenuSeq=0&searchType=&searchValue="
+        n+=1
+        response = requests.get(url, headers=headers)
+        soup=BeautifulSoup(response.content, 'html.parser')
+        noticeArea=soup.find('div', class_='tbody')
+        if (noticeArea):
+            if(len(noticeArea.find_all('ul')) == 4 ):break
+
+            for item in noticeArea.find_all('ul')[4:]:
+                noticedict = dict()
+                if (item.find('li', class_='black05 ellipsis') != None):
+                    li_list = item.find_all('li')
+                    title = item.find('li', class_='black05 ellipsis').find("a").get("title")
+                    link = item.find('li', class_='black05 ellipsis').find("a").get("data-params")
+                    link = "https://web.kangnam.ac.kr/menu/board/info/f19069e6134f8f8aa7f689a4a675e66f.do?scrtWrtiYn=false&encMenuSeq=%s&encMenuBoardSeq=%s" %(link[34:66],link[87:119])
+                    postdate = li_list[5].text
+                    division = li_list[2].text
+                    text = single_page_crawling(link,headers)
+                else:
+                    continue
+
+                #print(title,  link, postdate, division,text)
+                noticedict['Title'] = title
+                noticedict['Link'] =  link
+                noticedict['PostDate'] = postdate
+                noticedict['division'] = division
+                noticedict['NoticeType'] = '공지사항'
+                noticedict['Text'] = text
+                noticelist.append(noticedict)
+     
+    df = pd.DataFrame.from_records(noticelist)
+    return df
+
+def single_page_crawling(link, headers):
+    response = requests.get(link, headers=headers)
     soup=BeautifulSoup(response.content, 'html.parser')
     noticeArea=soup.find('div', class_='tbody')
+    text = ""
     if (noticeArea):
-        if(len(noticeArea.find_all('ul')) == 4 ):break
+        item = noticeArea.find_all('ul')[1]
+        text = item.find('div', class_= 'tbl_view cke_editable cke_editable_themed cke_contents_ltr').get_text()
 
-        for item in noticeArea.find_all('ul')[4:]:
-            noticedict = dict()
-            if (item.find('li', class_='black05 ellipsis') != None):
-                li_list = item.find_all('li')
-                title = item.find('li', class_='black05 ellipsis').find("a").get("title")
-                link = item.find('li', class_='black05 ellipsis').find("a").get("data-params")
-                link = "https://web.kangnam.ac.kr/menu/board/info/f19069e6134f8f8aa7f689a4a675e66f.do?scrtWrtiYn=false&encMenuSeq=%s&encMenuBoardSeq=%s" %(link[34:66],link[87:119])
-                postdate = li_list[5].text
-                division = li_list[2].text
-
-                response = requests.get(link, headers=headers)
-                soup=BeautifulSoup(response.content, 'html.parser')
-                noticeArea=soup.find('div', class_='tbody')
-
-
-
-            else:
-                continue
-
-            #print(title,  link, postdate, division)
-            noticedict['Title'] = title
-            noticedict['Link'] =  link
-            noticedict['PostDate'] = postdate
-            noticedict['division'] = division
-            noticedict['NoticeType'] = '공지사항'
-            noticelist.append(noticedict)
-
-print(noticelist)        
-#df = pd.DataFrame.from_records(noticelist)
-#print(df)
+    return text
+            
+all_pages_crawling().to_csv("dataframe.csv", mode='a', header=False, encoding='utf-8-sig')
