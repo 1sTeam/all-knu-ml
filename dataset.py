1b1c3926c1943f2d626d0c055ff166f1adc18e02
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-09-25 01:48:45 +0900

    text_spacing

--- dataset.py
+++ dataset.py
@@ -1,14 +1,23 @@
 import pandas as pd
-
-df_csv = pd.read_csv("dataframe.csv",encoding='utf-8')
+from pykospacing import Spacing
 
 #머신러닝 학습을 위한 공지사항 data 추출
+df_csv = pd.read_csv("dataframe.csv",encoding='utf-8')
 dataset = df_csv[['Binary','Title','Text']]
 
-#한 행씩 추출
-for index, row in dataset.iterrows():
-    print(row.Text)
+#테스트 데이터
+test_dataset = dataset.head(5)
+
+#텍스트 띄어쓰기 전처리
+def text_spacing(dataset):
+    spacing = Spacing()
+    for index, row in dataset.iterrows():
+        if(type(row.Text) == type("str")):
+            dataset.iloc[index,2] = spacing(row.Text)
+    return dataset
+    
 
 
+test_dataset = text_spacing(test_dataset)
 
-#print(dataset.head())
+
