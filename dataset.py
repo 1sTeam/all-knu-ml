12949cad2c1cee4c516ae4f05db293e6b1fa2d46
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-09-27 18:41:47 +0900

    데이터 셋 생성 완료

--- dataset.py
+++ dataset.py
@@ -11,22 +11,23 @@ from soynlp.normalizer import *
 df_csv = pd.read_csv("dataframe.csv",encoding='utf-8')
 dataset = df_csv[['Binary','Title','Text']]
 
-#테스트 데이터
-test_dataset = dataset.head(5)
+#데이터 셋
+dataset = dataset.head(1000)
 
 #텍스트 띄어쓰기 전처리
 def text_spacing(dataset):
     spacing = Spacing()
     for index, row in dataset.iterrows():
         if(type(row.Text) == type("str")):
-            dataset.iloc[index,2] = spacing(row.Text)
+            dataset.at[index,'Text'] = spacing(row.Text)
+            dataset.at[index,'Title'] = spacing(row.Title)
     return dataset
     
 def text_tokenazation(dataset):
     tokenizer = Okt()
 
     #토큰화 학습 (시간이 오래걸리나 안할시 성능 구려짐)
-    '''
+    
     urllib.request.urlretrieve("https://raw.githubusercontent.com/lovit/soynlp/master/tutorials/2016-10-20.txt", filename="2016-10-20.txt")
     corpus = DoublespaceLineCorpus("2016-10-20.txt")
     i = 0
@@ -40,10 +41,11 @@ def text_tokenazation(dataset):
     word_extractor = WordExtractor()
     word_extractor.train(corpus)
     word_score_table = word_extractor.extract()
-    '''
+    
     for index, row in dataset.iterrows():
         if(type(row.Text) == type("str")):
             dataset.at[index, 'Text'] = tokenizer.morphs(row.Text)
+            dataset.at[index, 'Title'] = tokenizer.morphs(row.Title)
 
     return dataset
 
@@ -51,11 +53,12 @@ def text_normalization(dataset):
     for index, row in dataset.iterrows():
         if(type(row.Text) == type("str")):
             dataset.at[index, 'Text'] = emoticon_normalize(row.Text, num_repeats=2)
-            dataset.at[index, 'Text'] = repeat_normalize(row.Text, num_repeats=2)
+            dataset.at[index, 'Title'] = repeat_normalize(row.Title, num_repeats=2)
+    return dataset
 
-test_dataset = text_spacing(test_dataset)
-test_dataset = text_normalization(test_dataset)
-test_dataset = text_tokenazation(test_dataset)
+dataset = text_spacing(dataset)
+dataset = text_normalization(dataset)
+dataset = text_tokenazation(dataset)
 
-print(test_dataset.Text)
+dataset.to_csv("dataset.csv", mode='a', header=False, encoding='utf-8-sig')
 
