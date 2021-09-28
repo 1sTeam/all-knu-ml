85acbbdc4abcac3f776e6aac04cc3aaf89a12b0e
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-09-28 16:40:09 +0900

    데이터셋 완성 및 필요 라이브러리 임포트

--- model.py
+++ model.py
@@ -1,10 +1,17 @@
-from numpy import dot
-from numpy.core.numeric import False_
-from numpy.linalg import norm
+#%%
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
+%matplotlib inline
+import matplotlib.pyplot as plt
+import urllib.request
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 dataset = pd.read_csv('dataset.csv', low_memory=False)
-print(dataset.head(2))
+# dataset['Text'] = dataset['Text'].fillna('')
+
+m_target = dataset['Binary'].to_numpy
+# m_dataset = np.array([dataset['Title'].to_numpy,dataset['Text'].to_numpy])
+m_dataset = dataset['Title'].to_numpy
+
+dataset['Binary'].value_counts().plot(kind='bar')
