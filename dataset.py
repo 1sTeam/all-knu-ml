d85101418fae676f09ce04ecf0bebe1768fc1ecb
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-09-29 16:49:15 +0900

    데이터셋 업데이트 및 사용자 사전 단어 추가

--- dataset.py
+++ dataset.py
@@ -6,6 +6,8 @@ import urllib.request
 from soynlp import DoublespaceLineCorpus
 from soynlp.word import WordExtractor
 from soynlp.normalizer import *
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 #머신러닝 학습을 위한 공지사항 data 추출
 df_csv = pd.read_csv("dataframe.csv",encoding='utf-8')
@@ -18,15 +20,36 @@ def text_normalization(dataset):
     dataset = dataset.dropna(how = 'any')
     dataset['Title'] = dataset['Title'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z ]","")
     dataset['Title'].replace('', np.nan, inplace=True)
-
     return dataset
 
 def text_tokenization(dataset):
-    komoran = Komoran(userdic = 'user_dictionary.txt')
-    print(komoran.morphs(dataset[1,1]))
+    komoran = Komoran(userdic= 'dic.txt')
+    temp = []
+    for sentence in dataset['Title']:
+        temp.append(komoran.nouns(sentence))
+    dataset['Title'] = temp
+    return dataset
+
+def text_integer(dataset):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(dataset['Title'])
+    token_size = len(tokenizer.word_index)
+
+    tokenizer = Tokenizer(token_size)
+    tokenizer.fit_on_texts(dataset['Title'])
+
+    dataset['Title'] = tokenizer.texts_to_sequences(dataset['Title'])
+    return dataset
+
+def text_len_pad(dataset):
+    max_len = 30
+    dataset['Title'] = pad_sequences(dataset['Title'], maxlen=max_len)
     return dataset
 
 dataset = text_normalization(dataset)
 dataset = text_tokenization(dataset)
-# dataset.to_csv("dataset.csv", mode='a', header=False, encoding='utf-8-sig')
+dataset = text_integer(dataset)
+dataset = text_len_pad(dataset)
+
+dataset.to_csv("dataset.csv", mode='a', header=False, encoding='utf-8-sig')
 
