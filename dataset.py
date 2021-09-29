d52b91310cf3c821fd917f034d0e799fd89305f8
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-09-29 15:07:55 +0900

    데이터 구조 변경

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import numpy as np
 from pykospacing import Spacing
-from konlpy.tag import Okt
+from ckonlpy.tag import Twitter
 import urllib.request
 from soynlp import DoublespaceLineCorpus
 from soynlp.word import WordExtractor
@@ -8,56 +9,29 @@ from soynlp.normalizer import *
 
 #머신러닝 학습을 위한 공지사항 data 추출
 df_csv = pd.read_csv("dataframe.csv",encoding='utf-8')
-dataset = df_csv[['Binary','Title','Text']]
+dataset = df_csv[['Binary','Title']]
 
 #데이터 셋
 dataset = dataset.head(1000)
 
-#텍스트 띄어쓰기 전처리
-def text_spacing(dataset):
-    spacing = Spacing()
-    for index, row in dataset.iterrows():
-        if(type(row.Text) == type("str")):
-            dataset.at[index,'Text'] = spacing(row.Text)
-            dataset.at[index,'Title'] = spacing(row.Title)
-    return dataset
-    
-def text_tokenazation(dataset):
-    tokenizer = Okt()
-
-    #토큰화 학습 (시간이 오래걸리나 안할시 성능 구려짐)
-    
-    urllib.request.urlretrieve("https://raw.githubusercontent.com/lovit/soynlp/master/tutorials/2016-10-20.txt", filename="2016-10-20.txt")
-    corpus = DoublespaceLineCorpus("2016-10-20.txt")
-    i = 0
-    for document in corpus:
-        if len(document) > 0:
-            print(document)
-            i = i+1
-        if i == 3:
-            break
-
-    word_extractor = WordExtractor()
-    word_extractor.train(corpus)
-    word_score_table = word_extractor.extract()
-    
-    for index, row in dataset.iterrows():
-        if(type(row.Text) == type("str")):
-            dataset.at[index, 'Text'] = tokenizer.morphs(row.Text)
-            dataset.at[index, 'Title'] = tokenizer.morphs(row.Title)
-
-    return dataset
-
 def text_normalization(dataset):
-    for index, row in dataset.iterrows():
-        if(type(row.Text) == type("str")):
-            dataset.at[index, 'Text'] = emoticon_normalize(row.Text, num_repeats=2)
-            dataset.at[index, 'Title'] = repeat_normalize(row.Title, num_repeats=2)
+    dataset = dataset.dropna(how = 'any')
+    dataset['Title'] = dataset['Title'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z ]","")
+    dataset['Title'].replace('', np.nan, inplace=True)
+    print(dataset.isnull().sum())
+
     return dataset
+def text_tokenization(dataset):
+    twitter = Twitter()
+    twitter.add_dictionary('비교과프로그램','Noun')
+    twitter.add_dictionary('집중학습지원','Noun')
+    twitter.add_dictionary('취창업지원센터','Noun')
+    twitter.add_dictionary('진로취창업센터','Noun')
+    twitter.add_dictionary('마음나눔센터','Noun')
+    twitter.add_dictionary('CTL','Noun')
+    twitter.add_dictionary('참인재','Noun')
+    twitter.add_dictionary('취업동아리','Noun')
+text_normalization(dataset)
 
-dataset = text_spacing(dataset)
-dataset = text_normalization(dataset)
-dataset = text_tokenazation(dataset)
-
-dataset.to_csv("dataset.csv", mode='a', header=False, encoding='utf-8-sig')
+# dataset.to_csv("dataset.csv", mode='a', header=False, encoding='utf-8-sig')
 
