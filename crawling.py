23e2bf740591775af06e43a7b31f4035e2f42ecc
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-09-30 11:32:07 +0900

    이진 분류를 위한 Title값 크롤링

--- crawling.py
+++ crawling.py
@@ -3,10 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def all_pages_crawling():
+def all_pages_crawling(n = 1):
     headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.62 Safari/537.36'}
     #1 부터 수집하면 오래걸림 공지사항이 494페이지까지 있어서 테스트용으로 490페이지부터 시작
-    n = 1
     noticelist = list()
 
     while(1):
@@ -55,6 +54,30 @@ def single_page_crawling(link, headers):
         text = item.find('div', class_= 'tbl_view cke_editable cke_editable_themed cke_contents_ltr').get_text()
 
     return text
-            
+
+def single_page_crawling_for_modeling():
+    headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.62 Safari/537.36'}
+    noticelist = list()
+
+    url = "https://web.kangnam.ac.kr/menu/f19069e6134f8f8aa7f689a4a675e66f.do?paginationInfo.currentPageNo=1&searchMenuSeq=0&searchType=&searchValue="
+    response = requests.get(url, headers=headers)
+    soup=BeautifulSoup(response.content, 'html.parser')
+    noticeArea=soup.find('div', class_='tbody')
+    if (noticeArea):
+        for item in noticeArea.find_all('ul')[4:]:
+            noticedict = dict()
+            if (item.find('li', class_='black05 ellipsis') != None):
+                li_list = item.find_all('li')
+                title = item.find('li', class_='black05 ellipsis').find("a").get("title")
+            else:
+                continue
+
+            #print(title,  link, postdate, division,text)
+            noticedict['Title'] = title
+            noticelist.append(noticedict)
+     
+    df = pd.DataFrame.from_records(noticelist)
+    return df
+
 #print(all_pages_crawling())
-all_pages_crawling().to_csv("dataframe.csv", mode='a', header=False, encoding='utf-8-sig')
+#all_pages_crawling().to_csv("dataframe.csv", mode='a', header=False, encoding='utf-8-sig')
--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 import urllib.request
+import dataset
 from konlpy.tag import Okt
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -12,12 +13,11 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.model_selection import train_test_split
 
-
 dataset = pd.read_csv('dataset.csv', low_memory=False)
 
 y_train = np.array(dataset['Binary'])
-x_train = []
 
+x_train = []
 for index in dataset['Title']:
   x_train.append(np.fromstring(index, dtype=int, sep=','))
 
@@ -27,8 +27,8 @@ x_train, x_test, y_train, y_test = train_test_split(
     x_train, y_train, test_size=0.2, random_state=1)
 
 model = Sequential()
-#model.add(Embedding(len(tokenizer.word_index), 100))
-#model.add(LSTM(128))
+model.add(Embedding(1024, 100))
+model.add(LSTM(128))
 model.add(Dense(1, activation='sigmoid'))
 
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
@@ -39,3 +39,4 @@ history = model.fit(x_train, y_train, epochs=100, callbacks=[es, mc], batch_size
 
 loaded_model = load_model('best_model.h5')
 print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(x_test, y_test)[1]))
+
