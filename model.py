354017ad67ed996318f20c19a9f97df329cd5cec
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-10-19 18:00:01 +0900

    인공 신경망 레이어 변경

--- model.py
+++ model.py
@@ -25,12 +25,14 @@ def create_predict(dataset):
   #     x_train, y_train, test_size=0.2, random_state=1)
 
   model = Sequential()
-  # model.add(Embedding(1152, 100))
-  # model.add(Bidirectional(LSTM(100)))
+  model.add(Embedding(1152, 100))
+  model.add(Bidirectional(LSTM(100)))
   # model.add(Dense(1, activation='relu'))
-  model.add(layers.Dense(16, activation='relu', input_shape=(len(dataset),)))
-  model.add(layers.Dense(16, activation='relu'))
-  model.add(layers.Dense(1, activation='sigmoid'))
+  model.add(Dense(1, activation='sigmoid'))
+
+  # model.add(layers.Dense(16, activation='relu', input_shape=(len(dataset),)))
+  # model.add(layers.Dense(16, activation='relu'))
+  # model.add(layers.Dense(1, activation='sigmoid'))
 
 
   es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
