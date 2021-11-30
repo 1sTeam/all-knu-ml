d971b946afe5b112c0682d700fa87453c3866bc4
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-11-30 21:04:09 +0900

    새로운 데이터 셋 학습 및 모델 검증

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from pandas.io.parsers import read_csv
 import dataset as dt
 import crawling as cl
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -14,38 +15,38 @@ from sklearn.model_selection import train_test_split
 import tensorflowjs as tfjs
 from pandas.plotting import scatter_matrix
 
-def create_predict(dataset):
-  dataset = pd.read_csv('dataset.csv', low_memory=False)
+def create_predict():
+  dataset = read_csv("dataset.csv")
   y_train = np.array(dataset['Binary'])
   x_train = []
   for index in dataset['Title']:
-    x_train.append(np.fromstring(index, dtype=int, sep=','))
+    x_train.append(np.fromstring(index, dtype=np.uint8, sep=','))
 
   x_train = pad_sequences(x_train, maxlen = 12)
-
+  print(x_train.shape)
   # x_train, x_test, y_train, y_test = train_test_split(
   #     x_train, y_train, test_size=0.2, random_state=1)
 
   model = Sequential()
 
-  model.add(Embedding(1152, 100))
-  model.add(LSTM(128))
-  model.add(Dense(1, activation='relu'))
-  # model.add(Dense(1, activation='sigmoid'))
+  # model.add(Embedding(1152, 100))
+  # model.add(LSTM(128))
+  # model.add(Dense(1, activation='relu'))
+  # # model.add(Dense(1, activation='sigmoid'))
 
-  # model.add(layers.Dense(16, activation='relu', input_shape=(len(dataset),)))
-  # model.add(layers.Dense(16, activation='relu'))
-  # model.add(layers.Dense(1, activation='sigmoid'))
+  model.add(layers.Dense(16, activation='relu', input_shape=(len(dataset),)))
+  model.add(layers.Dense(1, activation='sigmoid'))
 
 
-  es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
-  mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+  # es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+  # mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
 
   model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-  history = model.fit(x_train, y_train, epochs=15, callbacks=[es, mc], batch_size=30, validation_split=0.2)
-  # history = model.fit(x_train, y_train, epochs=30, batch_size=60, validation_split=0.2)
+  # history = model.fit(x_train, y_train, epochs=15, callbacks=[es, mc], batch_size=30, validation_split=0.2)
+  history = model.fit(x_train, y_train, epochs=30, batch_size=60, validation_split=0.2)
 
   # loaded_model = load_model('best_model.h5')
+  print("model 검증 정확도 : " + str(max(history.history['val_acc'])))
   return model
 
 
@@ -61,38 +62,23 @@ def sentiment_text_processing():
   
 def sentiment_predict(m_dataset, model):
   x_train = pad_sequences(m_dataset['Title'], maxlen = 12)
-
-  # for sentence in x_train:
-  score = model.predict(x_train)
-
-  print(score)
-
-  for s in score:
-    s = float(s)
-    if(s > 0.5):
-      print("{:.2f}% 확률로 비교과프로그램입니다.\n".format(s * 100))
+  for sentence in x_train:
+    score = float(model.predict(sentence))
+    if(score > 0.5):
+      print("{:.2f}% 확률로 비교과프로그램입니다.\n".format(score * 100))
     else:
-      print("{:.2f}% 확률로 비교과 프로그램이 아닙니다.\n".format((1 - s) * 100))
+      print("{:.2f}% 확률로 비교과 프로그램이 아닙니다.\n".format((1 - score) * 100))
 
-
-#전처리 된 데이터셋 불러오기
-tp = dt.text_processing(pd.read_csv('dataframe.csv', low_memory=False))
-tp.dataset = tp.dataset.head(1100)
-
-tp.text_normalization()
-tp.text_tokenization()
-tp.text_integer()
-scatter_matrix(tp.dataset, alpha=0.4, figsize=(12, 12), diagonal='kde')
-
-# visualize(dataset['Title'], dataset['Binary'])
-#모델 생성
-model = create_predict(tp.dataset)
+# # visualize(dataset['Title'], dataset['Binary'])
+# #모델 생성
+model = create_predict()
 
 #모델 불러오기
 # model = load_model('best_model.h5')
+
+# 모델 저장
 # model.save(filepath="m_model")
 # tfjs.converters.save_keras_model(model, "m_model_tfjs")
 
-
-# s_dataset = sentiment_text_processing()
-# sentiment_predict(s_dataset, model)
+s_dataset = sentiment_text_processing()
+sentiment_predict(s_dataset, model)
